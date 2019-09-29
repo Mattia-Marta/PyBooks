3325844196c87a1d544cc08275f2f28dbae04002
SeriousTia95 <mattiamarta.mm@gmail.com>
2019-09-30 00:01:21 +0200

    Updates

---
+++ Pattern Matching/StrongPasswordChecker.py
@@ -0,0 +1,8 @@
+'''
+from re import compile, search
+
+REGEX = compile(r'\d+[^0-9a-zA-Z]*[A-Z]+[^0-9a-zA-Z]*[a-z]+')
+
+
+def is_strong_password(password):
+    return len(password) >= 8 and bool(search(REGEX, ''.join(sorted(password))))
