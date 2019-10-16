3239827cbea4b94c62ad6cba968b51e318cb1826
Mattia Marta <mattiamarta.mm@gmail.com>
2019-10-16 10:53:24 +0200

    Exceptions

---
+++ Debugging/ExceptionsAndTraceback.py
@@ -0,0 +1,15 @@
+'''
+Writing code accounts for 90 percent of programming. Debugging code accounts for theother 90 percent.
+Exception is like "stop doing this and go to the exception statement"
+
+Exceptions are raised with "raise" statement, done like follow:
+• The raise keyword
+• A call to the Exception() function
+• A string with a helpful error message passed to the Exception() function
+
+'''
+
+#Raise exception example
+raise Exception('This is the error message that will appear.')
+
+
